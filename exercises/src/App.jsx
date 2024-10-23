6b7b112dd43cdb4f2a090be675eafd2f9694617b
Catherine Njenga <freshiah.catherine@gmail.com>
2024-10-23 11:10:23 +0300

    Update of the state is asynchronous

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -16,16 +16,22 @@ const App = () => {
   const [ left, setLeft ] = useState(0);
   const [ right, setRight ] = useState(0);
   const [ allClicks, setAll ] = useState([]);
-
+  const [ total, setTotal ] = useState(0);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'));
-    setClicks(left + 1);
+    console.log('left before', left);
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    console.log('left after', updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'));
-    setClicks(right + 1);
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   };
 
   return (
@@ -35,6 +41,7 @@ const App = () => {
       <button onClick={handleRightClick}>right</button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
