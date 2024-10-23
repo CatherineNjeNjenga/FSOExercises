fbed9819f7ba8614af2fd55ff2c92130f51f822f
Catherine Njenga <freshiah.catherine@gmail.com>
2024-10-23 10:15:26 +0300

    first commit

---
+++ exercises/src/App.jsx
@@ -0,0 +1,57 @@
+// import React from 'react'
+import { useState } from 'react';
+import './App.css'
+
+const Hello = ({name, age}) => {
+  console.log(name, age);
+
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()} </p>
+    </div>
+  )
+};
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick} >
+    {text}
+  </button>
+  )
+};
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0);
+  console.log('rendering with counter value', counter);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1)
+  };
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1)
+  };
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0)
+  };
+
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
+    </div>
+  )
+}
+
+export default App
---
+++ exercises/src/notes.md
@@ -0,0 +1,2 @@
+props is an object
+Often, several components need to reflect the same changing data. We recommend lifting the shared state up to their closest common ancestor.
