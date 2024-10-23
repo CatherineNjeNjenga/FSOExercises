3f0d43366bcaf94c8b3db92a37f3d2429b19a933
Catherine Njenga <freshiah.catherine@gmail.com>
2024-10-23 12:26:05 +0300

    Passing event handlers to child components

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -64,9 +64,9 @@ const App = () => {
       <History allClicks={allClicks} />
       <p>total {total}</p>
       <button onClick={hello('Catherine')}>button</button>
-      <button onClick={() => setToValue(1000)}>thousand</button>
-      <button onClick={() => setToValue(0)}>reset</button>
-      <button onClick={() => setToValue(value + 1)}>increment</button>
+      <Button handleClick={() => setToValue(1000)} text='thousand' />
+      <Button handleClick={() => setToValue(0)} text='reset'/>
+      <Button handleClick={() => setToValue(value + 1)} text='increment'/>
 
     </div>
   )
